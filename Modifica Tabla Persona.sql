77fa63f9fc3336ae5f8b67857f7128988788eebe
hvargas <henryvargastames@gmail.com>
2019-02-10 09:47:28 -0400

    Login and  Password  Encriptation

--- Modifica Tabla Persona.sql
+++
@@ -1,7 +0,0 @@
-Alter table usuario.persona add documento_identidad varchar(50);
-
-ALTER TABLE usuario.persona  
-ADD CONSTRAINT UNIQ_PER_DOC UNIQUE(documento_identidad);
-
-
-
--- inicializacion_Datos.sql
+++ inicializacion_Datos.sql
@@ -1,33 +1,48 @@
+/**********************************************************
+*	Version Inicial
+*
+*	Nombre				Fecha				Descripcion
+*	--------------------------------------------------------
+*	Desconocido
+*	Henry Vargas		09-02-2019			Modificacion de la tabla Person (ADD DOCUMENTO_IDENTIDAD) 
+*   Henry Vargas        10-02-2019          Aggregar datos a DOCUMENTO_IDENTIDAD
+*
+*************************************************************/
+
 USE SGSO_RESCUE;
 
 SET XACT_ABORT ON;
 SET NOCOUNT ON;
 
 BEGIN TRANSACTION;
-
-PRINT 'Insert data into the Person table...';
-
-INSERT INTO [Usuario].[Persona]([NOMBRE],[APELLIDO],[EMAIL],[DIRECCION],[TELEFONO])
-	 VALUES('Keegan','Guerrero','Curabitur.vel.lectus@feugiatSednec.com','Apartado núm.: 719, 2875 Praesent Avenida','372-76715')
-		,('Dante','Sandoval','consectetuer.mauris.id@Sed.edu','5652 Vel, C.','434-51410')
-		,('Michelle','Alvarado','Vivamus@egestasblandit.com','Apdo.:329-521 Eu Av.','772-41484')
-		,('Marvin','González','Sed.molestie.Sed@ametorciUt.net','9522 Interdum C.','200-47043')
-		,('Lance','Pérez','neque@idliberoDonec.net','Apartado núm.: 332, 6350 Nisl Carretera','123-64052')
-		,('John','Fernández','eu.erat@lacus.org','Apartado núm.: 768, 9826 Eros ','391-62813')
-		,('Buckminster','Gutiérrez','odio.a.purus@tristiquesenectus.com','406-6432 Velit. ','666-20426')
-		,('Brady','García','cursus@tellus.net','Apartado núm.: 404, 5769 Sit ','866-60718')
-		,('Keaton','Saavedra','lacus.Quisque@estmollis.co.uk','4861 Phasellus C.','963-62992')
-		,('Otto','Pizarro','magna.nec.quam@natoquepenatibus.edu','517-3064 Ac ','134-43427')
-		,('John','Zúñiga','dictum.magna@faucibus.edu','Apartado núm.: 177, 9479 Orci, Av.','975-70788')
-		,('Solomon','Bravo','id@ultricesposuerecubilia.net','Apartado núm.: 890, 4672 Placerat, Av.','332-56993')
-		,('Aidan','Vidal','aliquet.sem@rhoncusNullam.com','Apdo.:421-7765 Tincidunt Avenida','503-63588')
-		,('Drew','Vargas','Donec.egestas.Aliquam@vestibulumneceuismod.net','810-8652 Montes, Avda.','603-23165')
-		,('Stone','Alvarado','tellus.Aenean.egestas@loremegetmollis.net','346-8852 Dolor Ctra.','124-49130')
-		,('Hamish','Núñez','Vestibulum.accumsan.neque@penatibuset.org','622-676 Metus. Carretera','978-40317')
-		,('Miranda','Ortiz','egestas.Sed.pharetra@sapien.net','Apartado núm.: 774, 925 Erat Carretera','650-76702')
-		,('Lynn','Alarcón','primis.in.faucibus@montesnasceturridiculus.co.uk','Apdo.:756-5819 Euismod Av.','246-09372')
-		,('Alana','Castro','euismod@congueInscelerisque.co.uk','Apartado núm.: 571, 9041 Varius Calle','382-40511')
-		,('Lee','Rivera','Sed.eu@duiin.edu','Apartado núm.: 877, 5888 Dolor Avda.','133-94851');
+PRINT 'Alter Table Insert DOCUMENTO_IDENTIDAD into the Person table...';
+ALTER TABLE usuario.persona  
+ADD  DOCUMENTO_IDENTIDAD varchar(50) CONSTRAINT UNIQ_PER_DOC UNIQUE(DOCUMENTO_IDENTIDAD);
+PRINT 'Insert DOCUMENTO_IDENTIDAD into the Person table...';
+GO
+COMMIT TRANSACTION;
+BEGIN TRANSACTION;
+INSERT INTO [Usuario].[Persona]([NOMBRE],[APELLIDO],[EMAIL],[DIRECCION],[TELEFONO],[DOCUMENTO_IDENTIDAD])
+	 VALUES('Keegan','Guerrero','Curabitur.vel.lectus@feugiatSednec.com','Apartado núm.: 719, 2875 Praesent Avenida','372-76715','2640397')
+		,('Dante','Sandoval','consectetuer.mauris.id@Sed.edu','5652 Vel, C.','434-51410','5872275')
+		,('Michelle','Alvarado','Vivamus@egestasblandit.com','Apdo.:329-521 Eu Av.','772-41484','1893253')
+		,('Marvin','González','Sed.molestie.Sed@ametorciUt.net','9522 Interdum C.','200-47043','2160353')
+		,('Lance','Pérez','neque@idliberoDonec.net','Apartado núm.: 332, 6350 Nisl Carretera','123-64052','7250444')
+		,('John','Fernández','eu.erat@lacus.org','Apartado núm.: 768, 9826 Eros ','391-62813','1350617')
+		,('Buckminster','Gutiérrez','odio.a.purus@tristiquesenectus.com','406-6432 Velit. ','666-20426','7519471')
+		,('Brady','García','cursus@tellus.net','Apartado núm.: 404, 5769 Sit ','866-60718','6389963')
+		,('Keaton','Saavedra','lacus.Quisque@estmollis.co.uk','4861 Phasellus C.','963-62992','8201910')
+		,('Otto','Pizarro','magna.nec.quam@natoquepenatibus.edu','517-3064 Ac ','134-43427','1153097')
+		,('John','Zúñiga','dictum.magna@faucibus.edu','Apartado núm.: 177, 9479 Orci, Av.','975-70788','8491776')
+		,('Solomon','Bravo','id@ultricesposuerecubilia.net','Apartado núm.: 890, 4672 Placerat, Av.','332-56993','5681246')
+		,('Aidan','Vidal','aliquet.sem@rhoncusNullam.com','Apdo.:421-7765 Tincidunt Avenida','503-63588','3712493')
+		,('Drew','Vargas','Donec.egestas.Aliquam@vestibulumneceuismod.net','810-8652 Montes, Avda.','603-23165','8670416')
+		,('Stone','Alvarado','tellus.Aenean.egestas@loremegetmollis.net','346-8852 Dolor Ctra.','124-49130','6497038')
+		,('Hamish','Núñez','Vestibulum.accumsan.neque@penatibuset.org','622-676 Metus. Carretera','978-40317','5044715')
+		,('Miranda','Ortiz','egestas.Sed.pharetra@sapien.net','Apartado núm.: 774, 925 Erat Carretera','650-76702','9290350')
+		,('Lynn','Alarcón','primis.in.faucibus@montesnasceturridiculus.co.uk','Apdo.:756-5819 Euismod Av.','246-09372','8257529')
+		,('Alana','Castro','euismod@congueInscelerisque.co.uk','Apartado núm.: 571, 9041 Varius Calle','382-40511','6048626')
+		,('Lee','Rivera','Sed.eu@duiin.edu','Apartado núm.: 877, 5888 Dolor Avda.','133-94851','2019893');
 PRINT 'Person table done...';
 
 PRINT 'Insert data into the Cargo table...';
